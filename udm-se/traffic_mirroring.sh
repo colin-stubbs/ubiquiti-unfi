f706dc387f0aef8f75addf6caf59c2cf899f1501
Colin Stubbs <3059577+colin-stubbs@users.noreply.github.com>
2022-07-04 19:04:04 +1000

    Update traffic_mirroring.sh

--- udm-se/traffic_mirroring.sh
+++ udm-se/traffic_mirroring.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+SWITCH_SUBINT=1
 IDS_PORT="eth6"
 
-tc filter add dev switch0.1 parent ffff: protocol all u32 match u32 0 0 action mirred egress mirror dev ${IDS_PORT}
-tc -s -p filter ls dev switch0.1 parent ffff:
+# setup monitoring of the internal network via switch subinterface, e.g. switch0.1
+tc qdisc add dev switch0.${SWITCH_SUBINT} handle ffff: ingress
+tc -s qdisc ls dev switch0.${SWITCH_SUBINT}
+
+# direct traffic out the physical interface connected to the Corelight sensor
+tc filter add dev switch0.${SWITCH_SUBINT} parent ffff: protocol all u32 match u32 0 0 action mirred egress mirror dev ${IDS_PORT}
+tc -s -p filter ls dev switch0.${SWITCH_SUBINT} parent ffff:
 
 # EOF
