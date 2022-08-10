9bf14bd30d68a88cfe74143d6512a7e05e7c7b8f
Colin Stubbs <3059577+colin-stubbs@users.noreply.github.com>
2022-08-10 13:41:09 +1000

    Create install.sh

---
+++ udm-se/docker/install.sh
@@ -0,0 +1,23 @@
+apt-get remove docker docker-engine docker.io containerd runc
+apt-get update
+
+apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+systemctl enable docker.service
+
+docker ps
+
+# compose plugin manual install
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.7.0/docker-compose-linux-`arch` -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod 0755 $DOCKER_CONFIG/cli-plugins/docker-compose
